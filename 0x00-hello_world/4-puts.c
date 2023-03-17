2190f72c880975533bca6436a71cdfcc156754f3
CptRono <allanronoc@gmail.com>
2023-03-17 14:58:15 -0700

    script that passes a source file through preprocessing only

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: returns 0 if successfull
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
