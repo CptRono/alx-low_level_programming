d3340e07d0d3c84d45e1fcd0b93346683704b808
CptRono <allanronoc@gmail.com>
2023-03-19 05:56:04 -0700

    prints string into the standard error file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - this is the entry point
+ * Return: returns 1 to indicate the program has terminated with an error
+ * 2 - standard error
+ * 58 - tells write the number of characters to be written into the stderr file
+ */
+int main(void)
+{
+/*
+ * make an array of the string with the type char so that each character has
+ * a length of 1. the length of the array is the number of characters in the
+ * string
+ */
+	char string[] = "and that piece of art is useful\" - Dora Korpar, 2015\
+		-10-19\n";
+	int stringLength = sizeof(string);
+
+	write(2, string, stringLength);
+	return (1);
+}
