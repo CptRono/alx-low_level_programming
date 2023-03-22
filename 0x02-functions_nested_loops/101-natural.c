d80768e91c9b420106eba64fc5dcf5a51d6bd445
CptRono <allanronoc@gmail.com>
2023-03-22 14:00:41 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints sum of multiples of 3 & 5
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	b = 0;
+	for (a = 1; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b = b + a;
+		else
+			continue;
+	}
+	printf("%d", b);
+	return (0);
+}
