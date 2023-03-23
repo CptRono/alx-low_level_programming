c07efb631bbc749d5c40d51b690fa3b0a452955a
CptRono <allanronoc@gmail.com>
2023-03-22 17:53:03 -0700

    prints even fibonacci numbers below 4000000

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,28 +3,26 @@
 
 /**
  * main - prints fibonacci sequence
+ *
  * Return: 0 on success
  */
 int main(void)
 {
-	long int a;
+	long int sum;
 	long int b = 0;
 	long int c = 1;
 	long int d = c;
 
-	for (a = 0; d <= 4000000; d = c + b)
+	for (sum = 0; d <= 4000000; d = c + b)
 	{
 		if ((d % 2) == 0)
 		{
-			if (a == 0)
-				printf("%ld", d);
-			else
-				printf(", %ld", d);
-			a++;
+			sum += d;
 		}
 		b = c;
 		c = d;
 	}
-	printf("\n");
+	printf("%d\n", sum);
 	return (0);
 }
+
