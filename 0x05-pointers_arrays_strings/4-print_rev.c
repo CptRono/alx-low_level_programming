ccadc98626fb511b6c2f8b9b3ba0846fd3657c5e
CptRono <allanronoc@gmail.com>
2023-03-27 17:00:05 -0700

    reverses strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+/**
+ * print_rev - reverses strings
+ * @s: contains array of chars to be reversed
+*/
+void print_rev(char *s)
+{
+	int i = 0;
+
+	/*find length of string*/
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	/*print string from the back*/
+	for (i-- ; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
