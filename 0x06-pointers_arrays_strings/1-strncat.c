d5963ecee6337ffaea5d2b745c743cff8b97665b
CptRono <allanronoc@gmail.com>
2023-03-31 06:53:53 -0700

    header file fixes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * _strcat - appends the src string to the dest string, overwriting the
  * terminating null byte (\0) at the end of dest, and then adds a terminating
