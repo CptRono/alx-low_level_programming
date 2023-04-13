8de59984016b26c48435dba0a8bb895e0b6fd1a9
CptRono <allanronoc@gmail.com>
2023-04-13 22:47:33 +0000

    Fix last for loop

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		*((char *)p + i) = 0;
 	return (p);
 }
