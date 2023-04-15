d3d066a9239e9f7bef5c3b3fbe7ebbe67af19546
CptRono <allanronoc@gmail.com>
2023-04-15 23:07:34 +0000

    Prints number of program args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguments of the program
+ * @argc: number of arguments
+ * @argv: names of argyments
+ * Return: 0 on success
+*/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
