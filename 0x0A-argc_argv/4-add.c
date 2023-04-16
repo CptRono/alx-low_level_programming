e38fb887179426b4c8eec42c0c8e685de473a79d
CptRono <allanronoc@gmail.com>
2023-04-16 01:10:31 +0000

    Fix logical errors

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,24 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * is_int - checks if the chars in the string str are all integers
- * @str: str to be checked
- * Return: 1 if str is fully an int, 0 if not
- */
-int is_int(char *str)
-{
-	char *endptr;
-
-	if (str == NULL)
-		return (0);
-	/*check if str is an int*/
-	strtol(str, &endptr, 10);
-	/*if endpointer is the null terminator, then all chars are integers*/
-	if (*endptr != '\0')
-		return (0);
-	return (1);
-}
-/**
  * main - prints product of 2 args passed with the program
  * @argc: number of args
  * @argv: arguments passed
@@ -36,12 +18,16 @@ int main(int argc, char *argv[])
 	}
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) == 0 && argv[i][0] != 0)
+		char *endptr;
+		long num;
+
+		num = strtol(argv[i], &endptr, 10);
+		if (*endptr != '\0')
 		{
 			printf("Error\n");
-			return (1);
+			exit(1);
 		}
-		sum = sum + atoi(argv[i]);
+		sum = sum + (int)num;
 	}
 	printf("%d\n", sum);
 	return (0);
