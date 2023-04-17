e6f99f640362aa45b8da103327a94b39bb4d907d
CptRono <allanronoc@gmail.com>
2023-04-17 20:08:53 +0000

    Eliminate circular dependencies thru forward declaration

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include "dog.h"
 
+/*forward declarations to avoid circular dependencies*/
+/*circular dependencies are caused by header files including each other*/
+struct dog;
+typedef struct dog dog_t;
+
+/*normal fucntion declarations*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
