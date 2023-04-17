05f6382cfebc8ee87c640f8d7976193bb78ed7a8
CptRono <allanronoc@gmail.com>
2023-04-17 20:36:27 +0000

    Deletions in main.h

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include "main.h"
 #include "dog.h"
+#include "main.h"
 #include <stdlib.h>
 /**
  * init_dog - initializes a variable of type struct dog
@@ -17,4 +17,19 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		d->age = age;
 		d->owner = owner;
 	}
+}
+#include <stdio.h>
+#include "dog.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    struct dog my_dog;
+
+    init_dog(&my_dog, "Poppy", 3.5, "Bob");
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+    return (0);
 }
