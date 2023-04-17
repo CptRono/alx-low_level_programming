7ed7618cbc1147fc37f6a70326b7b8664a006d1b
CptRono <allanronoc@gmail.com>
2023-04-17 18:38:17 +0000

    Protect function in header from double inclusion

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - structure with name age and owner details of a dog
+ * @name: string of dog's name
+ * @age: age of dog
+ * @owner: owner
+*/
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
