34d2547997950afc793d00fd96dc7b92c6ff1a77
CptRono <allanronoc@gmail.com>
2023-04-17 20:12:59 +0000

    Consider NULL variables

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,10 +7,13 @@
  * @name: name to be copied
  * @age: age to be copied
  * @owner: owner to be copied
-*/
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL && name != NULL && owner != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
