7a64e7a6c64a3c076ed82766be030600d5744c3e
CptRono <allanronoc@gmail.com>
2023-04-17 20:38:26 +0000

    Deletions

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -2,15 +2,7 @@
 #define MAIN_H
 #include "dog.h"
 
-/*forward declarations to avoid circular dependencies*/
-/*circular dependencies are caused by header files including each other*/
-struct dog;
-typedef struct dog dog_t;
-
-/*normal fucntion declarations*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif
