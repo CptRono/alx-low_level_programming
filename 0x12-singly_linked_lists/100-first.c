0409b5f675c26e680b579328e34760dcc35df289
CptRono <allanronoc@gmail.com>
2023-04-29 16:41:30 +0000

    Betty style linter correction

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
  * print_b4_main - prints a string before main is executed
- * 
+ *
  * Description - uses a contructor attribute. this attribute specifies
  * a function to be called automatically before main function is
  * executed.
-*/
+ */
 void __attribute__((constructor)) print_b4_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	"I bore my house upon my back!\n");
 }
