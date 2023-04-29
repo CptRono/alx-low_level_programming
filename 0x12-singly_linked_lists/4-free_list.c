48d89b83ebe6cb5592f4568ffa837167b0c9bc08
CptRono <allanronoc@gmail.com>
2023-04-29 16:17:45 +0000

    Betty style

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,9 +2,14 @@
 #include <stdio.h>
 #include "lists.h"
 #include <string.h>
+/**
+ * free_list - frees a singly linked list
+ * @head: pointer to 1st node
+*/
 void free_list(list_t *head)
 {
 	list_t *tmp;
+
 	while (head != NULL)
 	{
 		/*assign tmp the next addr since it will be cleared*/
