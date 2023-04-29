5243794b1658eb7c413812fab4c35591b9988ddf
CptRono <allanronoc@gmail.com>
2023-04-29 13:05:54 +0000

    Fix logical errors and betty style

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,18 +6,18 @@
  * print_list - prints elements of a singly linked list
  * @h: pointer to the 1st node of the list
  * Return: retuns number of nodes
-*/
+ */
 size_t print_list(const list_t *h)
 {
 	int count;
 
-	for (count = 0; h->next != NULL; count++)
+	for (count = 0; h != NULL; count++)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		count++;
 	}
-	/*print last node also*/
-	printf("[%d] %s\n", h->len, h->str);
 	return (count);
 }
