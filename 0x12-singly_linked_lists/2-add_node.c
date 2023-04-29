f2b57979ad0bd2ec25fbe4cd47787ff721606c88
CptRono <allanronoc@gmail.com>
2023-04-29 13:27:37 +0000

    Betty coding style

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,9 +4,10 @@
 #include <string.h>
 /**
  * add_node -adds node to the begining of the list
+ * @head: pointer to 1st node
  * @str: string to be copied to new node
  * Return: the address of the new element, or NULL if it failed
-*/
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
@@ -18,12 +19,12 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new_node->str = malloc(strlen(str)+1);
-		if (new_node->str == NULL)
-		{
-			free(new_node);
-			return (NULL);
-		}
+	new_node->str = malloc(strlen(str) + 1);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	new_node->str = strcpy(new_node->str, str);
 	new_node->len = strlen(str);
 	new_node->next = *head;
