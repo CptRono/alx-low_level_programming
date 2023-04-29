8c2fec11b501921be36fb5bbf0c8c781dac392be
CptRono <allanronoc@gmail.com>
2023-04-29 13:25:16 +0000

    Fix compiling and logical errors

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,7 +13,17 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
+	{
+		free(new_node);
 		return (NULL);
+	}
+
+	new_node->str = malloc(strlen(str)+1);
+		if (new_node->str == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
 	new_node->str = strcpy(new_node->str, str);
 	new_node->len = strlen(str);
 	new_node->next = *head;
