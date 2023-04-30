1e96d3792b5b8a7de80018ac8c982a053d2977a9
CptRono <allanronoc@gmail.com>
2023-04-30 17:28:24 +0000

    Fix type & betty style

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,13 +2,17 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ * print_listint - prints elements in a list
+ * @h: pointer to 1st node
+ * Return: number of nodes
+*/
 size_t print_listint(const listint_t *h)
 {
 	int count;
 
-	if (h = NULL)
-		return 0;
+	if (h == NULL)
+		return (0);
 	for (count = 0; h != NULL; count++)
 	{
 		printf("%d\n", h->n);
