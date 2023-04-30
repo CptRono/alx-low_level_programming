3a30f7c440fb546cffc2f217f6fce267f0c244a6
CptRono <allanronoc@gmail.com>
2023-04-30 21:57:22 +0000

    Fix Betty coding style

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -6,19 +6,19 @@
  * sum_listint - returns sum of n in the listint_t linked list
  * @head: pointer to the 1st node
  * Return: sum of n in each node
-*/
+ */
 int sum_listint(listint_t *head)
 {
 	int sum;
 	listint_t *ptr;
 
 	if (head == NULL)
-		return(0);
+		return (0);
 	ptr = NULL;
 	ptr = head;
 	sum = 0;
 
-	while(ptr != NULL)
+	while (ptr != NULL)
 	{
 		sum = sum + ptr->n;
 		ptr = ptr->next;
