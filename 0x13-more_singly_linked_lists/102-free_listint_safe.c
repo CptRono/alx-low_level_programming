3ba7fd59c88076dc44aa5139ea3e1058fa45b892
CptRono <allanronoc@gmail.com>
2023-05-02 01:33:18 +0000

    Func that frees a list that may have a loop

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,80 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * find_loop2 - checks if list has a loop
+ * @head: pointer to 1st node of list
+ * Return: address of the 1st node of the loop. NULL if no loop
+ */
+
+listint_t *find_loop2(listint_t *head)
+{
+	size_t loop_found;
+	listint_t *fast, *slow, *tmp;
+
+	fast = slow = tmp = head;
+	loop_found = 0;
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow) /*loop detected*/
+		{
+			loop_found = 1;
+			break;
+		}
+	}
+	if (loop_found)
+	{
+		while (tmp != slow)
+		{
+			tmp = tmp->next;
+			slow = slow->next;
+		}
+		return (tmp);
+	}
+	return (NULL);
+}
+/**
+ * free_listint_safe - frees a list that may have a loop
+ * @h: pointer to pointer to head node
+ * Return: count of freed nodes
+*/
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *looped, *tmp;
+	size_t count;
+
+	if (*h == NULL)
+		return (0);
+	looped = find_loop2(*h);
+	tmp = *h;
+	if (looped == NULL)
+	{
+		for (count = 0; *h != NULL; count++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+	}
+	else
+	{
+		for (count = 1; *h != looped; count++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+		looped = looped->next;
+		for ( ; *h != looped; count++)
+		{
+			tmp = looped->next;
+			free(looped);
+			looped = tmp;
+		}
+	}
+	*h = NULL;
+	return (count);
+}
