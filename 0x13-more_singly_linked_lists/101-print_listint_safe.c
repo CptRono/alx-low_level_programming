f61e93de1087e20113bdd894c040625f8c8c194f
CptRono <allanronoc@gmail.com>
2023-05-02 21:20:07 +0000

    Fix node count returned in list without loop

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -63,7 +63,7 @@ size_t print_listint_safe(const listint_t *head)
 	/*if loop not found*/
 	if (looped == NULL)
 	{
-		for (count = 1; tmp != NULL; count++)
+		for (count = 0; tmp != NULL; count++)
 		{
 			printf("[%p] %d\n", (void *)tmp, tmp->n);
 			tmp = tmp->next;
