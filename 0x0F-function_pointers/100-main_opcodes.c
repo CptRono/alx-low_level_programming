471fd999c1382af1f4cdfb64ebdbb053e57a0792
CptRono <125222728+CptRono@users.noreply.github.com>
2023-05-03 16:45:16 +0300

    opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - prints opcodes of its own main function
+ * @argc: number of arguments passed with the program
+ * @argv: number of bytes to print
+ *
+ * Description - usage: ./main number_of_bytes
+ *	- creates a pointer to the main function and a char array
+ *	- copies the values of the main pointer into the char array
+ *	- whatever is copied into the array are the opcodes of main
+ *	- works because compiled machine code of a function are stored in a
+ * contiguous mem block immediately after the function's code
+ */
+void main(int argc, char *argv[])
+{
+	void (*main_ptr)(int, char **);
+	char *opcodes;
+	int args, count;
+
+	main_ptr = &main;
+	opcodes = (char *)main_ptr;
+	args = atoi(argv[1]);
+	if (argc != 2 || args == 0)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (args < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (count = 0; count < args; count++)
+	{
+		printf("%02x ", (unsigned char)*(opcodes + count));
+	}
+	printf("\n");
+}
